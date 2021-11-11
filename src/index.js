474ead4ff0341e21c5855785ee514833db6e5c7e
Kanzaki Taiki <w.j.81.best8+ccn@gmail.com>
2021-11-11 11:28:57 +0900

    DB接続の下準備を実施。

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-console.log("firest step!!");
-
+// サーバセットアップ
 const { setupServer } = require("./server");
 
 const server = setupServer();
@@ -9,3 +8,12 @@ server.listen(PORT, () => {
     console.log("baby diary API server started");
     console.log("===============================");
 });
+
+// DB設定
+
+// config.jsの中に記載してある設定情報を読み込む
+const config = require("./config");
+
+// データベースへの接続を初期化する。
+const knex = require("knex")(config.db);
+
